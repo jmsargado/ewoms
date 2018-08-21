13611caa586e52ac11c23b712d6152ba1bca60ed
Robert Kloefkorn <robert.kloefkorn@iris.no>
2018-08-21 21:11:53 +0200

    [cleanup][FVBaseDiscretizaion] space_ --> discreteFunctionSpace_ and other minor renamings.

--- ewoms/disc/common/fvbaselinearizer.hh
+++ ewoms/disc/common/fvbaselinearizer.hh
@@ -85,8 +85,6 @@ class FvBaseLinearizer
     typedef typename GET_PROP_TYPE(TypeTag, Stencil) Stencil;
     typedef typename GET_PROP_TYPE(TypeTag, ThreadManager) ThreadManager;
 
-    typedef typename GET_PROP_TYPE(TypeTag, DiscreteFunctionSpace) DiscreteFunctionSpace;
-
     typedef typename GET_PROP_TYPE(TypeTag, GridCommHandleFactory) GridCommHandleFactory;
 
     typedef Opm::MathToolbox<Evaluation> Toolbox;
@@ -111,9 +109,8 @@ class FvBaseLinearizer
 //! \endcond
 
 public:
-    FvBaseLinearizer(const DiscreteFunctionSpace& space)
-         : space_( space ),
-           jacobian_()
+    FvBaseLinearizer()
+        : jacobian_()
     {
         simulatorPtr_ = 0;
     }
@@ -547,7 +544,7 @@ private:
 
             // reset the column of the Jacobian matrix
             // put an identity matrix on the main diagonal of the Jacobian
-            jacobian_->unitRow( constraintDofIdx );
+            jacobian_->clearRow( constraintDofIdx, Scalar(1) );
 
             // make the right-hand side of constraint DOFs zero
             residual_[constraintDofIdx] = 0.0;
@@ -556,8 +553,6 @@ private:
 
     static bool enableConstraints_()
     { return GET_PROP_VALUE(TypeTag, EnableConstraints); }
-
-    const DiscreteFunctionSpace& space_;
 
     Simulator *simulatorPtr_;
     std::vector<ElementContext*> elementCtx_;
